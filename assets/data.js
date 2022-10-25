fa2f245b2146acca888d5cfec26b90651ccd2ca6
destinydinam <destinydinam1@gmail.com>
2022-10-25 14:24:24 +0000

    added url to each menu options title

--- assets/data.js
+++ assets/data.js
@@ -199,30 +199,37 @@ export const menuOptions = [
     options: [
       {
         title: 'Home',
+        url: 'home',
         icon: 'FiHome',
       },
       {
         title: 'Charts',
+        url: 'charts',
         icon: 'AiOutlineLineChart',
       },
       {
         title: 'Budget',
+        url: 'budget',
         icon: 'SlCalculator',
       },
       {
         title: 'Dates',
+        url: 'dates',
         icon: 'BsCalendar4Event',
       },
       {
         title: 'Metrics',
+        url: 'metrics',
         icon: 'TbPresentationAnalytics',
       },
       {
         title: 'Agency Cost',
+        url: 'agency-cost',
         icon: 'MdAttachMoney',
       },
       {
         title: 'Whiteboard',
+        url: 'whiteboard',
         icon: 'BiChalkboard',
       },
     ],
@@ -232,33 +239,38 @@ export const menuOptions = [
     options: [
       {
         title: 'Associates',
+        url: 'associates',
         icon: 'TbNurse',
       },
       {
         title: 'Physicians',
+        url: 'physicians',
         icon: 'CiStethoscope',
       },
       {
         title: 'Facilities',
+        url: 'facilities',
         icon: 'BsBuilding',
       },
       {
         title: 'Organization Payers',
+        url: 'organization-payers',
         icon: 'MdOutlinePayments',
       },
       {
         title: 'Referral Sources',
+        url: 'Referral-sources',
         icon: 'AiOutlineInfoCircle',
       },
     ],
   },
+
   {
     name: ' Patients',
     options: [
       {
         title: 'Add Referral/Patient',
         url: 'add-referral',
-        component: 'AddRefferal',
         icon: 'BsDot',
       },
       {
@@ -273,12 +285,12 @@ export const menuOptions = [
       },
       {
         title: 'Patient Encounters',
-        url: 'patient-encounters',
+        url: 'patients-encounters',
         icon: 'BsDot',
       },
       {
         title: 'Disaster Plans',
-        url: 'disaster-planning',
+        url: 'disaster-plans',
         icon: 'BsDot',
       },
       {
@@ -288,7 +300,7 @@ export const menuOptions = [
       },
       {
         title: 'Schedules Detail Listing',
-        url: 'schedules-detail-listing',
+        url: 'schedules-datail-lising',
         icon: 'BsDot',
       },
       {
@@ -318,7 +330,7 @@ export const menuOptions = [
       },
       {
         title: 'View/Print Orders',
-        url: 'view-print-orders',
+        url: 'view-orders',
         icon: 'BsDot',
       },
       {
@@ -333,54 +345,69 @@ export const menuOptions = [
     options: [
       {
         title: 'A/R Collection Assignments',
+        url: 'ar-collection-assignments',
         icon: 'BsDot',
       },
       {
         title: 'A/R Transaction Summary',
+        url: 'ar-transaction-summary',
         icon: 'BsDot',
       },
       {
         title: 'Agency Accounts',
+        url: 'agency-accounts',
         icon: 'BsDot',
       },
       {
         title: 'A/R Aging As Of Report',
+
+        url: 'ar-aging-as-of-report',
         icon: 'BsDot',
       },
       {
         title: 'Invoices',
+        url: 'invoices',
         icon: 'BsDot',
       },
       {
         title: 'Invoice Detail Report',
+        url: 'invoice-detail-report',
         icon: 'BsDot',
       },
       {
         title: 'Invoice Service Summary',
+        url: 'invoice-service-summary',
         icon: 'BsDot',
       },
       {
         title: 'Invoice Errors',
+        url: 'invoice-errors',
         icon: 'BsDot',
       },
       {
         title: 'ERA Payments',
+
+        url: 'era-payments',
         icon: 'BsDot',
       },
       {
         title: 'Payments',
+        url: 'payments',
         icon: 'BsDot',
       },
       {
         title: 'Add Paymnet',
+        url: 'add-payments',
         icon: 'BsDot',
       },
       {
         title: 'CAP Support  Data',
+        url: 'cap-support-data',
         icon: 'BsDot',
       },
       {
         title: 'PPS Billing Alerts',
+        url: 'pps-billing-alerts',
         icon: 'BsDot',
       },
     ],
@@ -390,62 +417,80 @@ export const menuOptions = [
     options: [
       {
         title: 'View  Physician Documents',
+        url: 'view-physcian-documents',
         icon: 'BsDot',
       },
       {
         title: 'Physician Document',
+
+        url: 'physcian-document',
         icon: 'BsDot',
       },
       {
         title: 'Associate Time Entry',
+        url: 'associate-time-entry',
         icon: 'BsDot',
       },
       {
         title: 'Oasis File Build',
+        url: 'oasis-file-build',
+
         icon: 'BsDot',
       },
       {
         title: 'Oasis Validation Post',
+
+        url: 'oasis-validation-post',
         icon: 'BsDot',
       },
       {
         title: 'Clinical Update Review',
+        url: 'clinical-update-review',
         icon: 'BsDot',
       },
       {
         title: 'Directions / Map',
+        url: 'directions',
         icon: 'BsDot',
       },
       {
         title: 'Home Health Patient Survey',
+        url: 'home-health-patient-survey',
         icon: 'BsDot',
       },
       {
         title: 'Hostpice Item Set file Build',
+        url: 'hospice-item-set-file-build',
         icon: 'BsDot',
       },
       {
         title: 'Hostpice Patient Survey',
+        url: 'hospice-patient-survey',
         icon: 'BsDot',
       },
       {
         title: 'IDT Review',
+        url: 'idt-review',
         icon: 'BsDot',
       },
       {
         title: 'Orders To Sign',
+        url: 'orders-to-sign',
         icon: 'BsDot',
       },
       {
         title: 'Pharmacy Import',
+        url: 'pharmacy-import',
         icon: 'BsDot',
       },
       {
         title: 'QA Review',
+        url: 'qa-review',
         icon: 'BsDot',
       },
       {
         title: 'RCD Document Gathering',
+        url: 'rcd-document-gathering',
         icon: 'BsDot',
       },
     ],
@@ -455,62 +500,78 @@ export const menuOptions = [
     options: [
       {
         title: 'View  Physician Documents',
+        url: 'view-physcian-documents',
         icon: 'BsDot',
       },
       {
         title: 'Physician Document',
+        url: 'physician-document',
         icon: 'BsDot',
       },
       {
         title: 'Associate Time Entry',
+        url: 'associate-time-entry',
         icon: 'BsDot',
       },
       {
         title: 'Oasis File Build',
+        url: 'oasis-file-build',
         icon: 'BsDot',
       },
       {
         title: 'Oasis Validation Post',
+
+        url: 'oasis-validation-post',
         icon: 'BsDot',
       },
       {
         title: 'Clinical Update Review',
+        url: 'clinical-update-review',
         icon: 'BsDot',
       },
       {
         title: 'Directions / Map',
+        url: 'directions',
         icon: 'BsDot',
       },
       {
         title: 'Home Health Patient Survey',
+        url: 'home-health-patient-survey',
         icon: 'BsDot',
       },
       {
         title: 'Hostpice Item Set file Build',
+        url: 'hospice-item-set-file-build',
         icon: 'BsDot',
       },
       {
         title: 'Hostpice Patient Survey',
+        url: 'hospice-patient-survey',
         icon: 'BsDot',
       },
       {
         title: 'IDT Review',
+        url: 'idt-review',
         icon: 'BsDot',
       },
       {
         title: 'Orders To Sign',
+        url: 'orders-to-sign',
         icon: 'BsDot',
       },
       {
         title: 'Pharmacy Import',
+        url: 'pharmacy-import',
         icon: 'BsDot',
       },
       {
         title: 'QA Review',
+        url: 'qa-review',
         icon: 'BsDot',
       },
       {
         title: 'RCD Document Gathering',
+        url: 'rcd-document-gathering',
         icon: 'BsDot',
       },
     ],
@@ -520,26 +581,32 @@ export const menuOptions = [
     options: [
       {
         title: 'Lookup Tables',
+        url: 'lookup-tables',
         icon: 'BsDot',
       },
       {
         title: 'Org/Region/Agency',
+        url: 'org-region-agency',
         icon: 'BsDot',
       },
       {
         title: 'Work Basket',
+        url: 'work-basket',
         icon: 'BsDot',
       },
       {
         title: 'Journey Entry',
+        url: 'journey-entry',
         icon: 'BsDot',
       },
       {
         title: 'Offline Forms App',
+        url: 'offline-forms-app',
         icon: 'BsDot',
       },
       {
         title: 'Note Forms Tables',
+        url: 'note-forms-tables',
         icon: 'BsDot',
       },
     ],
