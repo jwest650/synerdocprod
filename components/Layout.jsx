f36507b65fad70d8c1c2c3aac1411ecf88c94b47
destinydinam <destinydinam1@gmail.com>
2022-10-26 20:41:05 +0000

    added bg

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -11,7 +11,7 @@ const Layout = ({ children }) => {
     <div className={` ${darkTheme && 'dark'}`}>
       <Navbar />
       <Header />
-      <div className="flex bg-[#E9E6EB] pb-20 text-main-text-dark dark:bg-main-dark-bg">
+      <div className="flex bg-[#f6f8fc] pb-20 text-main-text-light dark:bg-main-dark-bg">
         {/* <Leftmenu /> */}
         {/* <div className="max-h-screen w-full overflow-scroll bg-main-light-bg pb-20 text-main-text-dark dark:bg-main-dark-bg"> */}
         {/* bg-[#0e1922] */}
