449bc24aa42dfbbecf67c23399ae11eec10b431f
Joseph Nartey <joenart2@gmail.com>
2022-10-27 12:13:07 +0000

    update: add referral page

--- pages/Patients/add-referral/index.jsx
+++ pages/Patients/add-referral/index.jsx
@@ -17,6 +17,7 @@ import {
   ModalCloseButton,
   Button,
   Input,
+  Checkbox,
 } from '@chakra-ui/react';
 import { useState } from 'react';
 
@@ -26,16 +27,18 @@ const AddReferral = () => {
 
   return (
     <div className='w-full bg-[#F6F8FC] p-10 text-gray-900'>
-      <div>
+      <section className=' mx-auto w-[80%]  '>
         <h1 className='text-2xl'>Create Referral</h1>
-      </div>
-      <section>
-        <div>
-          <p className='py-5'>Add a new referral into the system</p>
+        <hr />
+
+        <div className='py-4'>
+          <p>Add a new referral into the system</p>
+          <h1 className='text-2xl'>Agency</h1>
         </div>
-        <div className=' flex w-[500px] items-center justify-between'>
+        <hr />
+        <div className=' flex  items-center justify-between'>
           <div className='flex items-center justify-between'>
-            <label htmlFor='#' className='mr-5'>
+            <label htmlFor='#' className='mr-3'>
               Agency:{' '}
             </label>
             <Select size='xs'>
@@ -50,117 +53,319 @@ const AddReferral = () => {
               </option>
             </Select>
           </div>
-          <div className='flex items-center justify-between'>
-            <label htmlFor='#' className='mr-5'>
-              AgencyType:{' '}
+          <div className='my-2 flex'>
+            <label htmlFor='#' mr-3>
+              Agency Type:{' '}
             </label>
-            <Select size='xs'>
-              <option value='option1' className='text-gray-800'>
-                Option 1
-              </option>
-              <option value='option2' className='text-gray-800'>
-                Option 2
-              </option>
-              <option value='option3' className='text-gray-800'>
-                Option 3
-              </option>
-            </Select>
+            <div>
+              <Select size='xs'>
+                <option value='option1' className='text-gray-800'>
+                  Option 1
+                </option>
+                <option value='option2' className='text-gray-800'>
+                  Option 2
+                </option>
+                <option value='option3' className='text-gray-800'>
+                  Option 3
+                </option>
+              </Select>
+            </div>
           </div>
         </div>
       </section>
 
-      <section className='mt-5'>
-        <div className='flex'>
-          <label htmlFor='#'>Referral Source:</label>
-          <Select size='xs' maxWidth={500} marginRight={5} marginLeft={5}>
-            <option value='option1' className='text-gray-800'>
-              Option 1
-            </option>
-            <option value='option2' className='text-gray-800'>
-              Option 2
-            </option>
-            <option value='option3' className='text-gray-800'>
-              Option 3
-            </option>
-          </Select>
+      <section>
+        {/* Referral Source */}
+        <section className='mx-auto mt-5 w-[80%]'>
+          <div className=' py-4'>
+            <h1 className='text-2xl'>Referral Source</h1>
+          </div>
+          <hr />
+          <div className='flex py-3'>
+            <div className='w-[150px]'>
+              <label htmlFor='#'>Referral Source:</label>
+            </div>
+            <div>
+              <Select size='xs' marginRight={5}>
+                <option value='option1' className='text-gray-800'>
+                  Option 1
+                </option>
+                <option value='option2' className='text-gray-800'>
+                  Option 2
+                </option>
+                <option value='option3' className='text-gray-800'>
+                  Option 3
+                </option>
+              </Select>
+            </div>
 
-          <button
-            onClick={() => setOpenFindReferral(true)}
-            className='dark:text-[#E77654px-6 cursor-pointer rounded bg-[#E77654] py-1 px-5 text-sm capitalize text-[#fff]'
-          >
-            find referral source
-          </button>
-        </div>
+            <div className='ml-3'>
+              <button
+                onClick={() => setOpenFindReferral(true)}
+                className='dark:text-[#E77654px-6 cursor-pointer rounded bg-[#E77654] py-1 px-5 text-sm capitalize text-[#fff]'
+              >
+                find referral source
+              </button>
+            </div>
+          </div>
 
-        <div className='flex py-5'>
-          <h1 className='mr-5'>Referral Phone: </h1>
-          <p className='text-gray-500'>w: (3434)-343445</p>
-        </div>
+          <div className='flex py-5'>
+            <div className='w-[150px]'>
+              <h1 className='mr-5'>Referral Phone: </h1>
+            </div>
+            <p className='text-gray-500'>w: (3434)-343445</p>
+          </div>
 
-        <div className='flex '>
-          <h1 className='mr-5'>Referral History: </h1>
-          <TableContainer>
-            <Table variant='striped' colorScheme='gray' size={'sm'}>
-              <Thead backgroundColor={'#29507C'} color={'#fff'}>
-                <Tr>
-                  <Th color={'#fff'} isNumeric></Th>
-                  <Th color={'#fff'} isNumeric>
-                    30 days
-                  </Th>
-                  <Th color={'#fff'} isNumeric>
-                    60 days
-                  </Th>
-                  <Th color={'#fff'} isNumeric>
-                    90 days
-                  </Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                <Tr>
-                  <Td>Admits</Td>
-                  <Td isNumeric>0</Td>
-                  <Td isNumeric>0</Td>
-                  <Td isNumeric>0</Td>
-                </Tr>
-                <Tr>
-                  <Td>Non Admits</Td>
-                  <Td isNumeric>0</Td>
-                  <Td isNumeric>0</Td>
-                  <Td isNumeric>0</Td>
-                </Tr>
-              </Tbody>
-            </Table>
-          </TableContainer>
-        </div>
+          <div className='flex '>
+            <div className='w-[150px]'>
+              <h1 className=''>Referral History: </h1>
+            </div>
+            <TableContainer>
+              <Table variant='striped' colorScheme='gray' size={'sm'}>
+                <Thead backgroundColor={'#29507C'} color={'#fff'}>
+                  <Tr>
+                    <Th color={'#fff'} isNumeric></Th>
+                    <Th color={'#fff'} isNumeric>
+                      30 days
+                    </Th>
+                    <Th color={'#fff'} isNumeric>
+                      60 days
+                    </Th>
+                    <Th color={'#fff'} isNumeric>
+                      90 days
+                    </Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td>Admits</Td>
+                    <Td isNumeric>0</Td>
+                    <Td isNumeric>0</Td>
+                    <Td isNumeric>0</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Non Admits</Td>
+                    <Td isNumeric>0</Td>
+                    <Td isNumeric>0</Td>
+                    <Td isNumeric>0</Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </div>
 
-        <div className='mt-5 flex'>
-          <label htmlFor='#'>Mode of Delivery:</label>
-          <Select size='xs' maxWidth={500} marginRight={5} marginLeft={5}>
-            <option value='option1' className='text-gray-800'>
-              Option 1
-            </option>
-            <option value='option2' className='text-gray-800'>
-              Option 2
-            </option>
-            <option value='option3' className='text-gray-800'>
-              Option 3
-            </option>
-          </Select>
-        </div>
+          <div className='mt-5 flex'>
+            <div className='w-[150px]'>
+              <label htmlFor='#'>Mode of Delivery:</label>
+            </div>
+            <div>
+              <Select size='xs' marginRight={5}>
+                <option value='option1' className='text-gray-800'>
+                  Option 1
+                </option>
+                <option value='option2' className='text-gray-800'>
+                  Option 2
+                </option>
+                <option value='option3' className='text-gray-800'>
+                  Option 3
+                </option>
+              </Select>
+            </div>
+          </div>
 
-        <div className='my-5 flex'>
-          <label htmlFor='#'>Sales Rep:</label>
-          <Select size='xs' maxWidth={500} marginRight={5} marginLeft={5}>
-            <option value='option1' className='text-gray-800'>
-              Option 1
-            </option>
-            <option value='option2' className='text-gray-800'>
-              Option 2
-            </option>
-            <option value='option3' className='text-gray-800'>
-              Option 3
-            </option>
-          </Select>
+          <div className='my-5 flex'>
+            <div className='w-[150px]'>
+              <label htmlFor='#'>Sales Rep:</label>
+            </div>
+            <div>
+              <Select size='xs' marginRight={5}>
+                <option value='option1' className='text-gray-800'>
+                  Option 1
+                </option>
+                <option value='option2' className='text-gray-800'>
+                  Option 2
+                </option>
+                <option value='option3' className='text-gray-800'>
+                  Option 3
+                </option>
+              </Select>
+            </div>
+          </div>
+        </section>
+        {/* Referral Source Contact */}
+        <section className='mx-auto mt-10 w-[80%]'>
+          <div className='flex py-3'>
+            <div className='w-[150px]'>
+              <label htmlFor='#'>Referral Source Contact:</label>
+            </div>
+            <div>
+              <Select size='xs' marginRight={5}>
+                <option value='option1' className='text-gray-800'>
+                  Option 1
+                </option>
+                <option value='option2' className='text-gray-800'>
+                  Option 2
+                </option>
+                <option value='option3' className='text-gray-800'>
+                  Option 3
+                </option>
+              </Select>
+            </div>
+
+            <div className='ml-3'>
+              <button
+                onClick={() => setOpenFindReferral(true)}
+                className='dark:text-[#E77654px-6 cursor-pointer rounded bg-[#E77654] py-1 px-5 text-sm capitalize text-[#fff]'
+              >
+                add referral source contact
+              </button>
+            </div>
+          </div>
+
+          <div className='flex py-5'>
+            <div className='w-[150px]'>
+              <h1 className='mr-5'>Phone 1: </h1>
+            </div>
+            <div className='flex'>
+              <div>
+                <Select size='xs'>
+                  <option value='option1' className='text-gray-800'>
+                    Option 1
+                  </option>
+                  <option value='option2' className='text-gray-800'>
+                    Option 2
+                  </option>
+                  <option value='option3' className='text-gray-800'>
+                    Option 3
+                  </option>
+                </Select>
+              </div>
+              <p className='ml-3 text-gray-500'>(343)-343-445</p>
+              <div className='ml-3'>
+                <Input size={'xs'} />
+              </div>
+            </div>
+          </div>
+          <div className='flex py-5'>
+            <div className='w-[150px]'>
+              <h1 className='mr-5'>Phone 2: </h1>
+            </div>
+            <div className='flex'>
+              <div>
+                <Select size='xs'>
+                  <option value='option1' className='text-gray-800'>
+                    Option 1
+                  </option>
+                  <option value='option2' className='text-gray-800'>
+                    Option 2
+                  </option>
+                  <option value='option3' className='text-gray-800'>
+                    Option 3
+                  </option>
+                </Select>
+              </div>
+              <p className='ml-3 text-gray-500'>(343)-343-445</p>
+              <div className='ml-3'>
+                <Input size={'xs'} />
+              </div>
+            </div>
+          </div>
+          <div className='flex py-5'>
+            <div className='w-[150px]'>
+              <h1 className='mr-5'>Email: </h1>
+            </div>
+            <div>
+              <Input size={'xs'} />
+            </div>
+          </div>
+          <div className='flex py-5'>
+            <div className='w-[150px]'>
+              <h1 className='mr-5'>Information: </h1>
+            </div>
+            <div>
+              <Input size={'xs'} />
+            </div>
+          </div>
+        </section>
+      </section>
+
+      {/* Initial Referral Information */}
+      <section>
+        <div className=' mx-auto w-[80%] py-3 '>
+          <h1 className='py-3 text-2xl'>Initial Referral Information</h1>
+          <hr />
+          <div className='flex py-3'>
+            <div className='w-[150px]'>
+              <label htmlFor='#' className='mr-3'>
+                Payer Category:
+              </label>
+            </div>
+            <div>
+              <Select size='xs'>
+                <option value='option1' className='text-gray-800'>
+                  Option 1
+                </option>
+                <option value='option2' className='text-gray-800'>
+                  Option 2
+                </option>
+                <option value='option3' className='text-gray-800'>
+                  Option 3
+                </option>
+              </Select>
+            </div>
+          </div>
+          <div className='flex py-5'>
+            <div className='w-[150px]'>
+              <h1 className='mr-5'>Referral Date: </h1>
+            </div>
+            <div>
+              <Input size={'xs'} type='date' />
+            </div>
+          </div>
+          <div className='flex py-5'>
+            <div className='w-[150px]'>
+              <h1 className='mr-5'>Requested start of care: </h1>
+            </div>
+            <div>
+              <Input size={'xs'} type='date' />
+            </div>
+          </div>
+          <div className='flex py-5'>
+            <div className='w-[150px]'>
+              <h1 className='mr-5'>Admission Source: </h1>
+            </div>
+            <div>
+              <Select size='xs'>
+                <option value='option1' className='text-gray-800'>
+                  Option 1
+                </option>
+                <option value='option2' className='text-gray-800'>
+                  Option 2
+                </option>
+                <option value='option3' className='text-gray-800'>
+                  Option 3
+                </option>
+              </Select>
+            </div>
+          </div>
+          <div className='flex py-5'>
+            <div className='w-[150px]'>
+              <h1 className='mr-5'>Admission Type: </h1>
+            </div>
+            <div>
+              <Select size='xs'>
+                <option value='option1' className='text-gray-800'>
+                  Option 1
+                </option>
+                <option value='option2' className='text-gray-800'>
+                  Option 2
+                </option>
+                <option value='option3' className='text-gray-800'>
+                  Option 3
+                </option>
+              </Select>
+            </div>
+          </div>
         </div>
       </section>
 
@@ -217,14 +422,18 @@ const AddReferral = () => {
                 </Thead>
                 <Tbody>
                   <Tr>
-                    <Td>0</Td>
+                    <Td>
+                      <Checkbox defaultChecked></Checkbox>
+                    </Td>
                     <Td>0</Td>
                     <Td>0</Td>
                     <Td>0</Td>
                     <Td>0</Td>
                   </Tr>
                   <Tr>
-                    <Td> 0</Td>
+                    <Td>
+                      <Checkbox></Checkbox>
+                    </Td>
                     <Td>0</Td>
                     <Td>0</Td>
                     <Td>0</Td>
