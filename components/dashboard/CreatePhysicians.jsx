fa7af7d13c9336c478d1c3499a9c78a2624677df
jwest650 <jaywest650@gmail.com>
2022-11-05 13:33:33 +0000

    physicians update

--- components/dashboard/CreatePhysicians.jsx
+++ components/dashboard/CreatePhysicians.jsx
@@ -23,125 +23,90 @@ const CreatePhysicians = () => {
     };
 
     return (
-          <div>
-                <span
-                      onClick={onOpen}
-                      className="flex cursor-pointer items-center gap-1 rounded bg-orange-600 py-1 px-2 text-white"
-                >
-                      <BiPlus />
-                      <span className="text-sm">Create Associates</span>
-                </span>
-                <Modal isOpen={isOpen} "ize"{"3xl"} onClose={onClose}>
-                      <ModalOverlay />
-                      <ModalOverlay />
-                      <ModalContent>
-                              <div className="w-full rounded border-[2px] border-t-[20px] border-[#6b3e9280] p-4 pb-10 ">
-                                    <form
-                                          action=""
-                                          onSubmit={handleForm}
-                                          className=" space-y-4 p-5 capitalize"
-                                    >
-                                          <header className=" capitalize">
-                                                <h1 className="te
-                                  xt-2xl font-bold">
-                              
-                                                    add physicians
-                                              </h1>
-                                              <p>
-                                                    select a specialty, title or physician
-                                                      details""
-                                         
-                                              </p>
-                                     
-                                     
-                                  
-                                                </header>
-                                                <section className="flex items-center justify-between">
-                                                    <aside className=" space-y-3">
-                                                      {" "}
-                                                    <div className="flex items-center space-x-2">
-                                                        <label htmlFor="">
-                                                              <span className="text-orange-500">
+        <div>
+            <span
+                onClick={onOpen}
+                className="flex cursor-pointer items-center gap-1 rounded bg-orange-600 py-1 px-2 text-white"
+            >
+                <BiPlus />
+                <span className="text-sm">Create Physicians</span>
+            </span>
+            <Modal isOpen={isOpen} size={"3xl"} onClose={onClose}>
+                <ModalOverlay />
+                <ModalOverlay />
+                <ModalContent>
+                    <div className="w-full rounded border-[2px] border-t-[20px] border-[#6b3e9280] p-4 pb-10 ">
+                        <form
+                            action=""
+                            onSubmit={handleForm}
+                            className=" space-y-4 p-5 capitalize"
+                        >
+                            <header className=" capitalize">
+                                <h1 className="text-2xl font-bold">
+                                    add physicians
+                                </h1>
+                                <p>
+                                    select a specialty, title or physician
+                                    details
+                                </p>
+                            </header>
+                            <section className="flex items-center justify-between">
+                                <aside className=" space-y-3">
+                                    {" "}
+                                    <div className="flex items-center space-x-2">
+                                        <label htmlFor="">
+                                            <span className="text-orange-500">
                                                 *
-                                        
-                                             
-                                                 
-                                       </span>{
-               "                       "}
-                                 
-                                                            first name:
+                                            </span>
+                                            first name:
                                         </label>
-                
-                                                          <Input
-                                         
-                                                          w={200}
-                                     
-                                     
-                                  
-                                                                type="text"
-                                                                size="sm"
-                                                                placeholder="Enter first or lastname"
-                                                          />
-                                                    </div>
-                                                    <div className=" space-x-2">
-                                                          <label htmlFor="">middle:</label>{" "}
-                                        
-                                             
-                                               <I
-                      n               put
-                                     
-                                 
-                                                            w={200}
-                                                            type="text"
-                                                              size="sm"
-                                         
-                                                          placeho
-     l                                der="Ente
-      r                                first 
-                                  or lastname"
-                                                            />
-                                                        </div>
-                                                        <div className="space-x-2">
-                                                          <label htmlFor="">
-                                                            <span className="text-orange-500">
-                                                                *
-                                                              </span>{" "}
-                                        
-                                             
-                                                 
-                                       lastname
-                                 :    
-                                 
-                                                        </label>
-                                                        <Input
-                                                              w={200}
-                                        
-                                             
-                                                 
-                                       type="te
-                 x                    t"
-                                 
-                                                            size="sm"
-                                                          placeholder="Enter first or lastname"
-                                                      />
-                                     
-                                               
-                                  </div>
-                              
-                                                      <div className="flex items-center space-x-2">
-                                                        <label htmlFor="">title:</label>
-                                     
-                                                    <Select
-                                 
-                              
-                                                              placeholder="Select option"
-                                                            size="xs"
-                                                          w={120}
-                                                  >
-                                                  <option value="option1">
-                                                      Option 1
-                                                </option>
-                                              <option value="option2">
+
+                                        <Input
+                                            w={200}
+                                            type="text"
+                                            size="sm"
+                                            placeholder="Enter first or lastname"
+                                        />
+                                    </div>
+                                    <div className=" space-x-2">
+                                        <label htmlFor="">middle:</label>{" "}
+                                        <Input
+                                            n
+                                            put
+                                            w={200}
+                                            type="text"
+                                            size="sm"
+                                            placeho
+                                            l
+                                            der="Enter first or lastname"
+                                        />
+                                    </div>
+                                    <div className="space-x-2">
+                                        <label htmlFor="">
+                                            <span className="text-orange-500">
+                                                *
+                                            </span>{" "}
+                                            lastname :
+                                        </label>
+                                        <Input
+                                            w={200}
+                                            type="text"
+                                            size="sm"
+                                            placeholder="Enter first or lastname"
+                                        />
+                                    </div>
+                                    <div className="flex items-center space-x-2">
+                                        <label htmlFor="">title:</label>
+
+                                        <Select
+                                            placeholder="Select option"
+                                            size="xs"
+                                            w={120}
+                                        >
+                                            <option value="option1">
+                                                Option 1
+                                            </option>
+                                            <option value="option2">
                                                 Option 2
                                             </option>
                                             <option value="option3">
